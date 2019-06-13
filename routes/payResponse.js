c718eee5661cfb3057cbce956aab81408ad161a1
liyaxing <liyaxing@icarbonx.com>
2019-06-13 17:08:38 +0800

    删掉非必要代码

--- routes/payResponse.js
+++ routes/payResponse.js
@@ -1,14 +1,15 @@
 const express = require('express');
+const {getParams} = require('../utils/baseUtil');
+
 const router = express.Router()
-const routeParams = require('../utils/routeParams');
- 
+
  /*
   * 接受wxPay支付回调回来的数据
   * 同样的通知可能会多次发送给商户系统。商户系统必须能够正确处理重复的通知。
   * (遇到更新就更新数据库)
   */
  router.all('/wxPayResponse', function (req, res, next) {
-     var param = routeParams(req)
+     var param = getParams(req)
         res.json({errMsg:'success'})
      
  })
